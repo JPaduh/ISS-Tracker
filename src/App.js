cce5b33d55fc193a691d0d2c73f9077bd9e2e705
JohnPaduh <jwpaduh@gmail.com>
2023-07-31 18:40:28 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-// import './App.css';
-import { useState, useEffect } from 'react'
+// // import './App.css';
+// import { useState, useEffect } from 'react'
 
-function App() {
-    const [loading, setLoading] = useState(false)
-    const [longitude, setLongitude] = useState(0.1278)
-    const [latitude, setLatitude] = useState(51.5074)
+// function App() {
+//     const [loading, setLoading] = useState(false)
+//     const [longitude, setLongitude] = useState(0.1278)
+//     const [latitude, setLatitude] = useState(51.5074)
 
 
-  return (
-    <div className="App">
-      {!loading ? (
-        <Map center= {{ lat: latitude, lng: latitude}} />
-      ) : (
-        <h1>loading</h1>
-      )}
-    </div>
-  )
-}
+//   return (
+//     <div className="App">
+//       {!loading ? (
+//         <Map center= {{ lat: latitude, lng: latitude}} />
+//       ) : (
+//         <h1>loading</h1>
+//       )}
+//     </div>
+//   )
+// }
 
-export default App;
+// export default App;
